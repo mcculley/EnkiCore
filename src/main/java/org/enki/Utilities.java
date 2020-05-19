50eff82d297d76431bde15c7a46496f720e040bf
Gene McCulley <mcculley@stackframe.com>
2020-05-19 14:24:49 -0400

    Add some documentation comments.

--- src/main/java/org/enki/Utilities.java
+++ src/main/java/org/enki/Utilities.java
@@ -3,6 +3,9 @@ package org.enki;
 import java.time.Duration;
 import java.time.Instant;
 
+/**
+ * Utility functions that do not fit well elsewhere.
+ */
 public class Utilities {
 
     static {
@@ -13,6 +16,12 @@ public class Utilities {
         throw new AssertionError("static utility class is not intended to be instantiated");
     }
 
+    /**
+     * Sleep until a specified time, ignoring <code>InterruptedException</code>. The current thread will sleep at least
+     * until the supplied <code>Instant</code>.
+     *
+     * @param wakeupAt the <code>Instant</code> to wake up at
+     */
     public static void sleepUninterruptibly(final Instant wakeupAt) {
         Instant now = Instant.now();
         while (now.compareTo(wakeupAt) <= 0) {
@@ -26,6 +35,11 @@ public class Utilities {
         }
     }
 
+    /**
+     * Sleep for the specified duration, ignoring <code>InterruptedException</code>.
+     *
+     * @param duration the <code>Duration</code> to sleep
+     */
     public static void sleepUninterruptibly(final Duration duration) {
         sleepUninterruptibly(Instant.now().plus(duration));
     }
