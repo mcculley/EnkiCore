4de74817859f5682f7f880ca1edec8e9694d88b1
Gene McCulley <mcculley@stackframe.com>
2021-08-07 13:31:15 -0400

    Add CacheUtilities.

---
+++ src/main/java/org/enki/CacheUtilities.java
@@ -0,0 +1,59 @@
+package org.enki;
+
+import com.google.common.io.ByteSource;
+import com.google.common.io.ByteStreams;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.time.Duration;
+import java.time.Instant;
+
+public class CacheUtilities {
+
+    private CacheUtilities() {
+        throw new AssertionError("static utility class is not intended to be instantiated");
+    }
+
+    private static String makeFileName(final URL l) {
+        final String s = l.toString();
+        final StringBuilder buf = new StringBuilder();
+        for (final char c : s.toCharArray()) {
+            if (c == '/') {
+                buf.append('-');
+            } else {
+                buf.append(c);
+            }
+        }
+
+        return buf.toString();
+    }
+
+    private static InputStream openCachedURL(final URL l) throws IOException {
+        final File tmpDir = new File(System.getProperty("java.io.tmpdir"));
+        final File cachedFile = new File(tmpDir, makeFileName(l));
+        System.err.println("cachedFile=" + cachedFile);
+        if (cachedFile.exists()) {
+            final BasicFileAttributes attr = Files.readAttributes(cachedFile.toPath(), BasicFileAttributes.class);
+            final Duration age = Duration.between(attr.lastModifiedTime().toInstant(), Instant.now());
+            if (age.compareTo(Duration.ofDays(1)) < 0) {
+                System.err.println("using cached file");
+                return new FileInputStream(cachedFile);
+            } else {
+                System.err.println("cached file is expired. age=" + age);
+                cachedFile.delete();
+            }
+        }
+
+        System.err.println("fetching " + l);
+        final InputStream is = l.openStream();
+        final byte[] targetArray = ByteStreams.toByteArray(is);
+        Files.write(cachedFile.toPath(), targetArray);
+        return ByteSource.wrap(targetArray).openStream();
+    }
+
+}
