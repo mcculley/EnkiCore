6feb3d82600ea6ade8db35def0acdcbfe84ec4c1
Gene McCulley <mcculley@stackframe.com>
2023-01-19 16:47:10 -0500

    Add Predicates.

---
+++ src/main/java/org/enki/Predicates.java
@@ -0,0 +1,44 @@
+package org.enki;
+
+/*
+ * EnkiCore
+ *
+ * Copyright © 2023 Gene McCulley
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+public class Predicates {
+
+    private Predicates() {
+        throw new AssertionError("static utility class is not intended to be instantiated");
+    }
+
+    public static <T> Predicate<T> and(final Stream<Predicate<T>> s) {
+        return s.reduce(alwaysTrue(), Predicate::and);
+    }
+
+    public static <T> Predicate<T> alwaysTrue() {
+        return s -> true;
+    }
+
+}
