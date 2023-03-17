cbb995927b571d9eb75690a5d68379ce8ea57cfd
Gene McCulley <mcculley@stackframe.com>
2023-03-17 11:11:18 -0400

    Add ExcludeFromJacocoGeneratedReport.

---
+++ src/main/java/org/enki/core/ExcludeFromJacocoGeneratedReport.java
@@ -0,0 +1,15 @@
+package org.enki.core;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface ExcludeFromJacocoGeneratedReport {
+
+    // This is kind of a hack. Jacoco looks for an annotation with the string "Generated" in the name. We use this to
+    // annotate members which are not necessarily generated, but we want them excluded from the coverage analysis.
+
+}
