a109acabb9fc695fe53c2603836fd802e9aea9fc
Gene McCulley <mcculley@stackframe.com>
2023-03-20 11:54:05 -0400

    Tweak unit test.

--- src/test/java/org/enki/core/ConstrainedStringTest.java
+++ src/test/java/org/enki/core/ConstrainedStringTest.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test;
 import java.util.regex.Pattern;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ConstrainedStringTest {
 
@@ -39,6 +41,10 @@ public class ConstrainedStringTest {
     @Test
     public void testCallSign() {
         assertEquals("WDC4444", new CallSign("WDC4444").toString());
+        assertEquals(new CallSign("WDC4444"), new CallSign("WDC4444"));
+        assertNotEquals(new CallSign("K4444"), new CallSign("WDC4444"));
+        assertTrue(new CallSign("K4444").compareTo(new CallSign("WDC4444")) < 0);
+        assertThrows(IllegalArgumentException.class, () -> new CallSign(null));
         assertThrows(IllegalArgumentException.class, () -> new CallSign("@"));
         assertThrows(IllegalArgumentException.class, () -> new CallSign("a-3"));
         assertThrows(IllegalArgumentException.class, () -> new CallSign("a25491182"));
