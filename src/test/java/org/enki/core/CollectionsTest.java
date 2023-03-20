66603fff0aede608d7ce237a45ac34fafde5164d
Gene McCulley <mcculley@stackframe.com>
2023-03-20 08:09:35 -0400

    Add unit test.

--- src/test/java/org/enki/core/CollectionsTest.java
+++ src/test/java/org/enki/core/CollectionsTest.java
@@ -2,7 +2,11 @@ package org.enki.core;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -13,6 +17,24 @@ public class CollectionsTest {
         final List<Integer> l1 = List.of(1, 2, 3);
         final List<Integer> l2 = List.of(2, 3);
         assertEquals(l1, Collections.concat(1, l2));
+    }
+
+    @Test
+    public void testSortByValue() {
+        final Map<String, Integer> m = Map.of(
+                "apple", 5,
+                "pear", 3,
+                "melon", 6,
+                "squash", 1,
+                "banana", 3
+        );
+
+        final Set<Map.Entry<String, Integer>> s = Collections.sortByValue(m);
+        System.err.println(s);
+        assertEquals(m.size(), s.size());
+        final List<Map.Entry<String, Integer>> l = new ArrayList<>(s);
+        final List<Integer> values = l.stream().map(Map.Entry::getValue).collect(Collectors.toList());
+        assertEquals(values, values.stream().sorted().collect(Collectors.toList()));
     }
 
 }
